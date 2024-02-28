e88b8b7362ed0622235d8fb2d12cf46cd69124a9
Mehdi Fekih <sixfoursuited@gmail.com>
2024-02-28 14:23:59 +0000

    updated langchain to community

--- Dockerfile
+++ Dockerfile
@@ -10,7 +10,6 @@ RUN apt-get update && apt-get install -y \
     && rm -rf /var/lib/apt/lists/*
 
 COPY ./app .
-
 RUN pip3 install --upgrade pip
 RUN pip3 install -r requirements.txt
 
--- app/main.py
+++ app/main.py
@@ -11,10 +11,10 @@ from PyPDF2 import PdfReader
 # Custom/local modules
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
-from langchain.callbacks import get_openai_callback
+from langchain_community.llms import OpenAI
+from langchain_community.callbacks import get_openai_callback
 
 def main():
     # Load environment variables
